233f19ec5f435208970d497a77b9c12100d6a22b
Jonathan Ellström <jonathan.ellstrom@hotmail.com>
2022-10-08 15:35:26 +0200

    Fått till demo för att visa hero matchups vs Medusa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    matchups: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/matchups/vs-and-with/heroes?vsHeroes=MEDUSA&minimumAmountOfGamesForMatchup=0');
+    const body = await response.json();
+    this.setState({matchups: body});
+  }
+
+  render() {
+    const {matchups} = this.state;
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="App-intro">
+              <h2>matchups</h2>
+              {matchups.map(matchup =>
+                  <div key={matchup.heroName}>
+                    {matchup.heroName} - {matchup.winrateAdvantage} - ({matchup.matchCount})
+                  </div>
+              )}
+            </div>
+          </header>
+        </div>
+    );
+  }
 }
-
 export default App;
