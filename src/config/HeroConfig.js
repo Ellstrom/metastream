2ee0af4a926c700d83ac64d5422f2b385c0ec225
Jonathan Ellström <jonathan.ellstrom@hotmail.com>
2022-12-28 15:51:39 +0100

    Fixat heroconfig med alla heroes

--- src/config/HeroConfig.js
+++ src/config/HeroConfig.js
@@ -4,10 +4,370 @@ export const HeroConfig = {
             name: "Anti-Mage"
         },
         {
-            name: "Medusa"
+            name: "Axe"
+        },
+        {
+            name: "Bane"
+        },
+        {
+            name: "Bloodseeker"
+        },
+        {
+            name: "Crystal Maiden"
+        },
+        {
+            name: "Drow Ranger"
+        },
+        {
+            name: "Earthshaker"
+        },
+        {
+            name: "Juggernaut"
+        },
+        {
+            name: "Mirana"
+        },
+        {
+            name: "Morphling"
+        },
+        {
+            name: "Shadow Fiend"
+        },
+        {
+            name: "Phantom Lancer"
+        },
+        {
+            name: "Puck"
+        },
+        {
+            name: "Pudge"
+        },
+        {
+            name: "Razor"
+        },
+        {
+            name: "Sand King"
+        },
+        {
+            name: "Storm Spirit"
+        },
+        {
+            name: "Sven"
+        },
+        {
+            name: "Tiny"
+        },
+        {
+            name: "Vengeful Spirit"
+        },
+        {
+            name: "Windranger"
         },
         {
             name: "Zeus"
+        },
+        {
+            name: "Kunkka"
+        },
+        {
+            name: "Lina"
+        },
+        {
+            name: "Lion"
+        },
+        {
+            name: "Shadow Shaman"
+        },
+        {
+            name: "Slardar"
+        },
+        {
+            name: "Tidehunter"
+        },
+        {
+            name: "Witch Doctor"
+        },
+        {
+            name: "Lich"
+        },
+        {
+            name: "Riki"
+        },
+        {
+            name: "Enigma"
+        },
+        {
+            name: "Tinker"
+        },
+        {
+            name: "Sniper"
+        },
+        {
+            name: "Necrophos"
+        },
+        {
+            name: "Warlock"
+        },
+        {
+            name: "Beastmaster"
+        },
+        {
+            name: "Queen of Pain"
+        },
+        {
+            name: "Venomancer"
+        },
+        {
+            name: "Faceless Void"
+        },
+        {
+            name: "Wraith King"
+        },
+        {
+            name: "Death Prophet"
+        },
+        {
+            name: "Phantom Assassin"
+        },
+        {
+            name: "Pugna"
+        },
+        {
+            name: "Templar Assassin"
+        },
+        {
+            name: "Viper"
+        },
+        {
+            name: "Luna"
+        },
+        {
+            name: "Dragon Knight"
+        },
+        {
+            name: "Dazzle"
+        },
+        {
+            name: "Clockwerk"
+        },
+        {
+            name: "Leshrac"
+        },
+        {
+            name: "Nature's Prophet"
+        },
+        {
+            name: "Lifestealer"
+        },
+        {
+            name: "Dark Seer"
+        },
+        {
+            name: "Clinkz"
+        },
+        {
+            name: "Omniknight"
+        },
+        {
+            name: "Enchantress"
+        },
+        {
+            name: "Huskar"
+        },
+        {
+            name: "Night Stalker"
+        },
+        {
+            name: "Broodmother"
+        },
+        {
+            name: "Bounty Hunter"
+        },
+        {
+            name: "Weaver"
+        },
+        {
+            name: "Jakiro"
+        },
+        {
+            name: "Batrider"
+        },
+        {
+            name: "Chen"
+        },
+        {
+            name: "Spectre"
+        },
+        {
+            name: "Ancient Apparition"
+        },
+        {
+            name: "Doom"
+        },
+        {
+            name: "Ursa"
+        },
+        {
+            name: "Spirit Breaker"
+        },
+        {
+            name: "Gyrocopter"
+        },
+        {
+            name: "Alchemist"
+        },
+        {
+            name: "Invoker"
+        },
+        {
+            name: "Silencer"
+        },
+        {
+            name: "Outworld Destroyer"
+        },
+        {
+            name: "Lycan"
+        },
+        {
+            name: "Brewmaster"
+        },
+        {
+            name: "Shadow Demon"
+        },
+        {
+            name: "Lone Druid"
+        },
+        {
+            name: "Chaos Knight"
+        },
+        {
+            name: "Meepo"
+        },
+        {
+            name: "Treant Protector"
+        },
+        {
+            name: "Ogre Magi"
+        },
+        {
+            name: "Undying"
+        },
+        {
+            name: "Rubick"
+        },
+        {
+            name: "Disruptor"
+        },
+        {
+            name: "Nyx Assassin"
+        },
+        {
+            name: "Naga Siren"
+        },
+        {
+            name: "Keeper of the Light"
+        },
+        {
+            name: "Io"
+        },
+        {
+            name: "Visage"
+        },
+        {
+            name: "Slark"
+        },
+        {
+            name: "Medusa"
+        },
+        {
+            name: "Troll Warlord"
+        },
+        {
+            name: "Centaur Warrunner"
+        },
+        {
+            name: "Magnus"
+        },
+        {
+            name: "Timbersaw"
+        },
+        {
+            name: "Bristleback"
+        },
+        {
+            name: "Tusk"
+        },
+        {
+            name: "Skywrath Mage"
+        },
+        {
+            name: "Abaddon"
+        },
+        {
+            name: "Elder Titan"
+        },
+        {
+            name: "Legion Commander"
+        },
+        {
+            name: "Techies"
+        },
+        {
+            name: "Ember Spirit"
+        },
+        {
+            name: "Earth Spirit"
+        },
+        {
+            name: "Underlord"
+        },
+        {
+            name: "Terrorblade"
+        },
+        {
+            name: "Phoenix"
+        },
+        {
+            name: "Oracle"
+        },
+        {
+            name: "Winter Wyvern"
+        },
+        {
+            name: "Arc Warden"
+        },
+        {
+            name: "Monkey King"
+        },
+        {
+            name: "Dark Willow"
+        },
+        {
+            name: "Pangolier"
+        },
+        {
+            name: "Grimstroke"
+        },
+        {
+            name: "Hoodwink"
+        },
+        {
+            name: "Void Spirit"
+        },
+        {
+            name: "Snapfire"
+        },
+        {
+            name: "Mars"
+        },
+        {
+            name: "Dawnbreaker"
+        },
+        {
+            name: "Marci"
+        },
+        {
+            name: "Primal Beast"
         }
     ]
 };
