07337894e47e5191fd06357034bce7ea9ff631d8
Jonathan Ellström <jonathan.ellstrom@hotmail.com>
2022-12-28 19:01:51 +0100

    rensat bort oanvänd kod

--- src/App.js
+++ src/App.js
@@ -5,32 +5,10 @@ import { FilterContext } from "./components/FilterContext";
 import HeroPage from "./components/HeroPage";
 
 class App extends Component {
-  state = {
-    isLoading: false,
-    matchups: [],
-    mapName: "abc"
-  };
-
-  async componentDidMount() {
-    /*
-    this.setState({isLoading: true});
-    const response = await fetch('/matchups/vs-and-with/heroes?vsHeroes=MEDUSA&minimumAmountOfGamesForMatchup=0');
-    const body = await response.json();
-    this.setState({matchups: body});
-    this.setState({isLoading: false});
-     */
-  }
-
   render() {
-    const {matchups} = this.state;
-    return (
-        <div className="App">
+      return (
+          <div className="App">
           <header className="App-header">
-            {this.state.isLoading &&
-                <div>
-                  Sidan laddar, lugna ne mä för fan!!
-                </div>
-            }
             <div className="App-intro">
                 <HeroPage/>
             </div>
--- src/components/HeroSelection.js
+++ src/components/HeroSelection.js
@@ -1,32 +1,7 @@
-import React, {useContext, useState} from 'react'
+import React from 'react'
 import {Col, Container, Row} from 'react-bootstrap'
-import { FilterContext } from "./FilterContext";
 
 const HeroSelection = (props) => {
-
-
-    const {
-        setSearchContent,
-        setSmokeVisible,
-        setFlashVisible,
-        setMolotovVisible,
-        setGrenadeVisible,
-        setCTVisible,
-        setTVisible,
-        setStratVisible,
-        setAVisible,
-        setMidVisible,
-        setBVisible,
-        setYardVisible,
-        setRampVisible,
-        setVentVisible,
-        setValueOneVisible,
-        setValueTwoVisible,
-        setValueThreeVisible,
-        setValueFourVisible,
-        setValueFiveVisible
-    } = useContext(FilterContext);
-
     return (
         <Container className="hero-selection">
             <Row>
